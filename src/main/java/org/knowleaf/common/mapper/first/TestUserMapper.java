0ac8ef2db122d3ef6f10c11607d87148a0822e18
LJJ <liujianjian@booksroo.com>
2019-06-23 10:50:19 +0800

    mybatis example

--- src/main/java/org/knowleaf/common/mapper/first/TestUserMapper.java
+++ src/main/java/org/knowleaf/common/mapper/first/TestUserMapper.java
@@ -1,9 +1,22 @@
 package org.knowleaf.common.mapper.first;
 
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
 import org.knowleaf.common.domain.first.TestUser;
+import org.knowleaf.common.domain.first.TestUserQuery;
 
 public interface TestUserMapper {
+    long countByExample(TestUserQuery example);
+
+    int deleteByExample(TestUserQuery example);
+
     int insert(TestUser record);
 
     int insertSelective(TestUser record);
+
+    List<TestUser> selectByExample(TestUserQuery example);
+
+    int updateByExampleSelective(@Param("record") TestUser record, @Param("example") TestUserQuery example);
+
+    int updateByExample(@Param("record") TestUser record, @Param("example") TestUserQuery example);
 }
